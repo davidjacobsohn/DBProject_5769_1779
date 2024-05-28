d9ded4779e944d04bc2b16ebc781df4ea240ae66
davidjacobsohn <154846793+davidjacobsohn@users.noreply.github.com>
2024-05-28 16:25:36 +0300

    Delete insertTables.sql

--- insertTables.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Units (unit_id_, unit_name_, location_) VALUES (1, 'Golani', 'North');
-INSERT INTO Units (unit_id_, unit_name_, location_) VALUES (2, 'Givati', 'South');
-INSERT INTO Units (unit_id_, unit_name_, location_) VALUES (3, 'Paratroopers', 'Central');
-select * from units
