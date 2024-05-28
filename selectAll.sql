85f2e864b702b0d63b0189910880b8697a4cab19
davidjacobsohn <154846793+davidjacobsohn@users.noreply.github.com>
2024-05-28 16:26:05 +0300

    Delete selectAll.sql

--- selectAll.sql
+++
@@ -1,9 +0,0 @@
-select * from units;
-select * from Salaries;
-select * from Trainings_;
-select * from Roles_;
-select * from Projects;
-select * from Soldiers;
-select * from Participant;
-select * from Have;
-
